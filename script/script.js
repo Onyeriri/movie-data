3875ec5edd254d0a2e820ae4983452d6d516dd66
Onyeriri Victor <viktorchinonso@gmail.com>
2022-09-14 23:25:47 +0100

    Added more feature to html section

--- script/script.js
+++ script/script.js
@@ -2,8 +2,8 @@ import movieData from "../movieStore.js";
 
 const mission = {
   "Mission Impossible Part 2": {
-    plot: "",
-    cast: [],
+    plot: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium consectetur repudiandae reprehenderit minima deserunt accusantium quisquam harum reiciendis temporibus perspiciatis rem atque ullam voluptatibus tenetur quas ad, alias quo eum.",
+    cast: ["Victor", "Titi"],
     runtime: 2,
     rating: 5,
     year: 1991,
@@ -12,4 +12,30 @@ const mission = {
 
 movieData["Mission Impossible Part 2"] = mission["Mission Impossible Part 2"];
 
-console.log(movieData);
+const MovieArray = Object.values(movieData);
+const MovieNames = Object.keys(movieData);
+let plot, cast, runtime, rating, year, name;
+MovieArray.map((value, key) => {
+  name = MovieNames[key];
+  plot = value.plot;
+  cast = value.cast.map((value) => value + " ");
+  runtime = value.runtime;
+  rating = value.rating;
+  year = value.year;
+
+  console.log(
+    "Title: " + name,
+    "\n",
+    "Plot: " + plot,
+    "\n",
+    "Cast: " + cast,
+    "\n",
+    "Runtime: " + runtime,
+    "\n",
+    "Rating: " + rating,
+    "\n",
+    "Year: " + year
+  );
+});
+
+console.log(MovieArray);
